462fe3c154338435b9340ca4cef845c0a964c875
wacawa <mikkunmitsuaki@gmail.com>
2022-05-06 19:21:58 +0900

    ステップ1-3終了。PRに挑戦。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,4 @@ class User < ApplicationRecord
   :recoverable, :rememberable, :validatable, :confirmable
   
   validates :name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true, length: {minimum: 8}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
   devise_for :users
+  devise_scope :user do
+    get '/users', to: 'devise/registrations#new'
+  end
+  
   root 'static_pages#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
